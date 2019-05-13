8c9e0ee9b2740d6494ffbb9460e90365fb21b817
Ibragimkariev <ibrosh_96@mail.ru>
2019-05-13 14:15:26 +0600

    ver-6

---
+++ js/script.js
@@ -0,0 +1,7 @@
+//
+// $('.nav-li').on('click', function (e) {
+//     $('.category-items').toggleClass('menu-active');
+// });
+// $('.nav-li a').on('click', function (e) {
+//     $('.nav-li').removeClass('menu-active');
+// });
