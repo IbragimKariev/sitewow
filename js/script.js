e1c4775b1b03292805cea8550c63995e23c03ca0
Ibragimkariev <ibrosh_96@mail.ru>
2019-05-17 13:37:51 +0600

    ver-7

--- js/script.js
+++ js/script.js
@@ -5,3 +5,24 @@
 // $('.nav-li a').on('click', function (e) {
 //     $('.nav-li').removeClass('menu-active');
 // });
+
+
+
+document.body.onclick = function (event) {
+    event = event || window.event;
+    let allDiv = document.querySelectorAll('.image');
+    console.log(allDiv);
+    for (let i=0; i< allDiv.length; i++){
+        allDiv[i].classList.remove('active');
+    }
+
+    if (event.target.classList.contains('image')){
+        document.getElementById('max').src = event.target.src;
+
+
+        event.target.classList.add('active');
+    }
+
+}
+
+
