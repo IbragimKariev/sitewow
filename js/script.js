75b67442efa14e7e3f8c1e1927bc3fcdf3adeb4c
Ibragimkariev <ibrosh_96@mail.ru>
2019-05-20 14:31:52 +0600

    ver-8

--- js/script.js
+++ js/script.js
@@ -1,28 +1,2 @@
-//
-// $('.nav-li').on('click', function (e) {
-//     $('.category-items').toggleClass('menu-active');
-// });
-// $('.nav-li a').on('click', function (e) {
-//     $('.nav-li').removeClass('menu-active');
-// });
-
-
-
-document.body.onclick = function (event) {
-    event = event || window.event;
-    let allDiv = document.querySelectorAll('.image');
-    console.log(allDiv);
-    for (let i=0; i< allDiv.length; i++){
-        allDiv[i].classList.remove('active');
-    }
-
-    if (event.target.classList.contains('image')){
-        document.getElementById('max').src = event.target.src;
-
-
-        event.target.classList.add('active');
-    }
-
-}
 
 
