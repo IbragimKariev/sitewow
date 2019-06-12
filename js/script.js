49cff261d779432166f1fcd3ef3bbad4c5de89d8
Ibragim <ibrosh_96@mail.ru>
2019-06-12 17:08:47 +0600

    addBTN

--- js/script.js
+++ js/script.js
@@ -1,2 +1,9 @@
+$( "#burger" ).click(function check() {
+    $( this ).toggleClass( "burgeractive");
+});
 
 
+$('.btn-menu').on('click', function (e) {
+    $('.btn-menu').toggleClass('menu-active');
+    $('.nav-mini').toggleClass('menu-active');
+});
